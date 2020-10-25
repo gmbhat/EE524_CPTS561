e7903fe9adebd97556b423b4c1ce3d9810ea35a5
Ganapati Bhat <ganapati.bhat@wsu.edu>
2020-10-25 13:03:54 -0700

    Make changes for PA2

--- 401.bzip2/runGem5.sh
+++ 401.bzip2/runGem5.sh
@@ -3,4 +3,4 @@ export GEM5_DIR=/net/fs/gbhat/pub/gem5
 export BENCHMARK=./src/benchmark
 export ARGUMENT="./data/input.program 10"
 export OUTPUT_DIR=./m5out # Set the output directory according to your preference.
-time $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_DIR $GEM5_DIR/configs/example/se.py -c $BENCHMARK -o "$ARGUMENT" -I 100000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=1MB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=1 --cacheline_size=64
+time $GEM5_DIR/build/X86/gem5.opt -d $OUTPUT_DIR $GEM5_DIR/configs/example/se.py -c $BENCHMARK -o "$ARGUMENT" -I 500000000 --cpu-type=TimingSimpleCPU --caches --l2cache --l1d_size=128kB --l1i_size=128kB --l2_size=1MB --l1d_assoc=2 --l1i_assoc=2 --l2_assoc=4 --cacheline_size=64
